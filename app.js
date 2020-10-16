7f436b3b1596c3a318255a1523aeae1f17877a03
5xians <786828962@qq.com>
2020-10-16 19:56:22 +0800

    update

---
+++ app.js
@@ -0,0 +1,22 @@
+const exec = require('child_process').execSync
+const fs = require('fs')
+const rp = require('request-promise')
+const download = require('download')
+
+// 公共变量
+const URL = process.env.URL
+
+async function downFile () {
+    let file = await download(URL, './')
+    await fs.writeFileSync('./app.js',file, 'utf8')
+}
+
+async function start() {
+  // 下载最新代码
+  await downFile();
+  console.log('下载代码完毕')
+  // 执行
+  await exec("node app.js");
+  console.log('执行完毕')
+}
+start()
