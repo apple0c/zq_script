d4977cee7a0f0970bb51d7138bb821494ac430c6
5xians <786828962@qq.com>
2020-10-16 21:27:15 +0800

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const child_process = require('child_process')
+const exec = require('child_process').execSync
 const fs = require('fs')
 const rp = require('request-promise')
 const download = require('download')
@@ -10,26 +10,13 @@ async function downFile() {
     let file = await download(URL, './')
     await fs.writeFileSync('./app.js', file, 'utf8')
 }
-const workerProcess = child_process.exec('node app.js', function (error, stdout, stderr) {
-    if (error) {
-        console.log(error.stack);
-        console.log('Error code: ' + error.code);
-        console.log('Signal received: ' + error.signal);
-    }
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-});
 
 async function start() {
     // 下载最新代码
     await downFile();
     console.log('下载代码完毕')
     // 执行
-    
-    workerProcess.on('exit', function (code) {
-        console.log('子进程已退出，退出码 ' + code);
-    });
-    //await child_process.exec("node app.js");
+    await exec("node app.js");
     console.log('执行完毕')
 }
 start()
