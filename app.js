fb27c478e5aa457ab912736a0be51e83cfb73ba3
5xians <786828962@qq.com>
2020-10-16 20:36:09 +0800

    update

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const download = require('download')
 // 公共变量
 const URL = process.env.URL
 
-async function downFile () {
+async function downFile() {
     let file = await download(URL, './')
-    await fs.writeFileSync('./app.js',file, 'utf8')
+    await fs.writeFileSync('./app.js', file, 'utf8')
 }
 
 async function start() {
-  // 下载最新代码
-  await downFile();
-  console.log('下载代码完毕')
-  // 执行
-  await exec("node app.js");
-  console.log('执行完毕')
+    // 下载最新代码
+    await downFile();
+    console.log('下载代码完毕')
+    // 执行
+    await exec("node app.js");
+    console.log('执行完毕')
 }
 start()
